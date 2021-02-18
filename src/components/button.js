2239704bacc4be68b375bd6abed9a5a2b4221b67
unknown <vitorsinedino@gmail.com>
2021-02-18 16:53:18 -0300

    added bootstrap

---
+++ src/components/button.js
@@ -0,0 +1,17 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import React from "react"
+
+const Button = ({children}) =>{
+  return(
+    <button type="button" className="btn btn-primary" >Test</button>
+  )
+}
+
+
+export default Button
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Button from "../components/button"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hello World</h1>
+    <p>this is vitor on gatsby.</p>
     <p>Now go build something great.</p>
+    <button>test</button>
+    <Button></Button>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
