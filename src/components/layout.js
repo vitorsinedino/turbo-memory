afc1d6b14766f8c39b3f0af417124356f04a6f1a
unknown <vitorsinedino@gmail.com>
2021-02-19 14:56:12 -0300

    navbar adittion

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Nav from "./nav";
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,6 +26,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Nav></Nav>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import Button from "../components/button"
+
 
 const IndexPage = () => (
   <Layout>
@@ -13,7 +13,6 @@ const IndexPage = () => (
     <p>this is vitor on gatsby.</p>
     <p>Now go build something great.</p>
     <button>test</button>
-    <Button></Button>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
